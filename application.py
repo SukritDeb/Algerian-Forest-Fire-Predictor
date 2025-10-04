5c7e65f9f8d606a4f6001a7cdc0250cca789fea7
SukritDeb <debsukrit00@gmail.com>
2025-10-04 19:20:34 +0530

    some major changes

--- application.py
+++ application.py
@@ -15,5 +15,12 @@ standard_scaler = pickle.load(open('models/scaler.pkl', 'rb'))
 def index():
     return render_template('index.html')
 
+@app.route('/predict', methods=['GET', 'POST'])
+def predict_datapoint():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('home.html')
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
